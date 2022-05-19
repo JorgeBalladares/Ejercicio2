e8a8e784aa75649abc6394f0f38ffb3e7b66705e
JorgeBalladares <105714153+JorgeBalladares@users.noreply.github.com>
2022-05-19 12:02:06 +0200

    Add files via upload

--- src/main/java/Ejercicio1/Principal.java
+++ src/main/java/Ejercicio1/Principal.java
@@ -1,18 +1,18 @@
 package Ejercicio1;
 
+
+import lombok.Data;
+import lombok.Getter;
+
 import java.io.FileReader;
 import java.io.IOException;
-
+@Getter
 public class Principal {
     public static void main(String[] args) throws IOException {
     leerFichero l = new leerFichero();
     l.leer();
-
-
     }
 }
-
-
 
 
 
